ab0a8eee3e2b2e344d3a7f2de9eb6ffb31b82bdc
Gabriel Queiroz <gabrie.queiroz@labsit.io>
2020-10-16 12:45:07 -0300

    Ajustar campos

--- src/pages/PokemonDetails/index.js
+++ src/pages/PokemonDetails/index.js
@@ -1,28 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import {Container, Wrapper} from './styles';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Wrapper } from "./styles";
 
-const PokemonDetails = ({location}) => {
-
+const PokemonDetails = ({ location }) => {
   const [pokemon, setPokemon] = useState({});
-
+  console.log(pokemon);
   const getPokemon = async (url) => {
     try {
       const { data } = await axios.get(url);
-      setPokemon(data)
-      console.log("pokemon: "+pokemon);
-      console.log("data: "+data);
+      setPokemon(data);
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   useEffect(() => {
     const url = location.search.substr(1);
     getPokemon(url);
-    console.log("url: " +url);
-    // eslint-disable-next-line
-  }, [location.search])
+  }, []);
 
   return (
     <Container>
@@ -32,7 +27,7 @@ const PokemonDetails = ({location}) => {
             <div className="card-header">
               <div className="row">
                 <div className="col-5">
-                  <h5>{ pokemon.id }</h5>
+                  <h5>{pokemon.id}</h5>
                 </div>
                 <div className="col-7">
                   <div className="float-right">
@@ -45,15 +40,26 @@ const PokemonDetails = ({location}) => {
             <div className="card-body">
               <div className="row align-items-center">
                 <div className="col-3">
-                  <img src="{ pokemon.url.sprites['front_default'] }" className="card-img-top rounded mx-auto mtt-2" />
+                  {pokemon.id && (
+                    <img
+                      src={pokemon.sprites.back_default}
+                      className="card-img-top rounded mx-auto mtt-2"
+                    />
+                  )}
                 </div>
                 <div className="col-9">
-                  <h4 className="mx-auto">{ pokemon.name}</h4>
+                  <h4 className="mx-auto">{pokemon.name}</h4>
                   <div className="row align-items-center">
                     <div className="col-12 col-md-3">HP</div>
                     <div className="col-12 col-md-9">
                       <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
                           <small>100</small>
                         </div>
                       </div>
@@ -63,7 +69,13 @@ const PokemonDetails = ({location}) => {
                     <div className="col-12 col-md-3">Attack</div>
                     <div className="col-12 col-md-9">
                       <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
                           <small>100</small>
                         </div>
                       </div>
@@ -73,7 +85,13 @@ const PokemonDetails = ({location}) => {
                     <div className="col-12 col-md-3">Defense</div>
                     <div className="col-12 col-md-9">
                       <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
                           <small>100</small>
                         </div>
                       </div>
@@ -83,7 +101,13 @@ const PokemonDetails = ({location}) => {
                     <div className="col-12 col-md-3">Speed</div>
                     <div className="col-12 col-md-9">
                       <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
                           <small>100</small>
                         </div>
                       </div>
@@ -93,7 +117,13 @@ const PokemonDetails = ({location}) => {
                     <div className="col-12 col-md-3">Sp. Atk.</div>
                     <div className="col-12 col-md-9">
                       <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
                           <small>100</small>
                         </div>
                       </div>
@@ -103,21 +133,18 @@ const PokemonDetails = ({location}) => {
                     <div className="col-12 col-md-3">Sp. Def.</div>
                     <div className="col-12 col-md-9">
                       <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        <div
+                          className="progress-bar"
+                          role="progressbar"
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        >
                           <small>100</small>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-
-              <div className="row mt-1">
-                <div className="col">
-                  <br />
-                  <p className="text-center">
-                  Aqui é onde ficará a descrição de cada pokémon.
-                  </p>
                 </div>
               </div>
             </div>
@@ -127,30 +154,33 @@ const PokemonDetails = ({location}) => {
             <div className="card-body">
               <h5 className="card-title text-center">Profile</h5>
               <div className="row">
-                <div className="col-md-6">
-                  <div className="row">
-                    <div className="col-6">
-                      <h6 className="float-right">Altura:</h6>
-                    </div>
-                    <div className="col-6">
-                      <h6 className="float-left">Altura aqui</h6>
-                    </div>
-                    <div className="col-6">
-                      <h6 className="float-right">Peso:</h6>
-                    </div>
-                    <div className="col-6">
-                      <h6 className="float-left">Peso aqui</h6>
-                    </div>
-                  </div>
-                </div>
+                <div className="col-md-6"></div>
                 <div className="col-md-6">
                   <div className="row">
                     <div className="col-6">
                       <h6 className="float-right">Abilities:</h6>
                     </div>
+                    {pokemon.id && (
+                      <div className="col-6">
+                        <h6 className="float-left">
+                          {pokemon.abilities.map(
+                            (item) => `${item.ability.name}, `
+                          )}
+                        </h6>
+                      </div>
+                    )}
+
                     <div className="col-6">
-                      <h6 className="float-left">Abilities aqui</h6>
+                      <h6 className="float-right">Types:</h6>
                     </div>
+                    {pokemon.id && (
+                      <div className="col-6">
+                        <h6 className="float-left">
+                          {pokemon.types.map((item) => `${item.type.name}, `)}
+                        </h6>
+                      </div>
+                    )}
+
                     <div className="col-6">
                       <h6 className="float-right">EVs:</h6>
                     </div>
@@ -163,15 +193,16 @@ const PokemonDetails = ({location}) => {
             </div>
 
             <div className="card-footer text-muted">
-              Dados consumidos da <a href="https://pokeapi.co/" target="_blank">PokeAPI.co</a>
+              Dados consumidos da{" "}
+              <a href="https://pokeapi.co/" target="_blank">
+                PokeAPI.co
+              </a>
             </div>
-
           </div>
-
         </div>
       </Wrapper>
     </Container>
   );
-}
+};
 
 export default PokemonDetails;
